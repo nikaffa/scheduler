1afc8e214e06cffe075ddc4ac062671364392f43
Veronika Fatikhova <fidelio@list.ru>
2021-12-08 18:46:31 -0800

    implement InterviewerListItem

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import classNames from "classnames"; //utility to join classNames together
+import "components/InterviewerListItem.scss";
+
+const InterviewerListItem = (props) => {
+  //appends a class to the item if a prop's value is true, using classNames utility
+  const interviewerClass = classNames(
+    "interviewers__item", {
+    "interviewers__item--selected": props.selected
+    });
+
+  return (
+    <li className={interviewerClass} onClick={() => props.setInterviewer(props.id)}>
+      <img className="interviewers__item-image" src={props.avatar} alt={props.name}/>
+      {props.selected && props.name}
+    </li>
+  );
+}
+ 
+export default InterviewerListItem;
+
