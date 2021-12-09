2af47109bd750dfb1db409f847bcb5cb8105892a
Veronika Fatikhova <fidelio@list.ru>
2021-12-08 20:29:28 -0800

    implement InterviewerList

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewerList.scss";
+
+const InterviewerList = (props) => {
+  //renders multiple components using map 
+  const newList = props.interviewers.map(interviewer => 
+    <InterviewerListItem 
+    key={interviewer.id} 
+    name={interviewer.name}
+    avatar = {interviewer.avatar}
+    selected={interviewer.id === props.interviewer}
+    setInterviewer={props.setInterviewer}
+    /> 
+  );
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{newList}</ul>
+    </section>  
+  );
+}
+ 
+export default InterviewerList;
+
