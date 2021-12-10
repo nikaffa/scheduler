7ba82dd3d379c0ac6486e801efc0507cfddd754d
Veronika Fatikhova <fidelio@list.ru>
2021-12-09 17:02:15 -0800

    implement Displaying Appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,19 +1,28 @@
 import React from "react";
+import Empty from "./Empty";
+import Header from "./Header";
+import Show from "./Show";
 import "./styles.scss";
 
+
 const Appointment = (props) => {
-  const showAppointment = () => {
-    let text ="";
-    if (props.time) {
-      text = `Appointment at ${props.time}`;
-    } else {
-      text = "No appointments";
-    }
-    return text;
-  }
+  // const showAppointment = () => {
+  //   let text ="";
+  //   if (props.time) {
+  //     text = `Appointment at ${props.time}`;
+  //   } else {
+  //     text = "No appointments";
+  //   }
+  //   return text;
+  // }
   
   return (
-    <article className="appointment">{showAppointment()}</article>
+    <article className="appointment">
+      <Header time={props.time}/>
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name}/> : <Empty />}
+      
+    </article>
+  
   );
 }
  
