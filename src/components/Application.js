15b9a664761b5c7640c8a8dbf5da0c4208f934ef
Veronika Fatikhova <fidelio@list.ru>
2021-12-11 22:42:43 -0800

    implement Retrieving Appointments in Application

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,62 +6,29 @@ import DayList from "./DayList";
 import Appointment from "./Appointment";
 import { getAppointmentsForDay } from "helpers/selectors";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
-
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    // you may put the line below, but will have to remove/comment hardcoded appointments variable
     appointments: {}
   });
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
   const setDay = day => setState(prev => ({ ...prev, day }));
-  const setDays = days => setState(prev => ({ ...prev, days }));
 
-  const newAppointments = appointments.map(app => 
+  const newAppointments = dailyAppointments.map(app => 
     <Appointment key={app.id} {...app}/>
   );
 
   useEffect(() => {
-    axios.get('/api/days').then((response) => {
-      setDays((response.data));
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+    ])
+    .then((all) => {
+      setState(prev => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data}));
     });
   }, []);
 
@@ -78,7 +45,7 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList 
           days={state.days}
-          day={state.day}
+          value={state.day}
           onChange={setDay}
           />
         </nav>
