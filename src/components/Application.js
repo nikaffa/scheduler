4cee64f67a8dc22a2538b97a4f3114edbddfdc6f
Veronika Fatikhova <fidelio@list.ru>
2021-12-12 12:16:30 -0800

    Implement fetching Interviewer Data and test

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,31 +4,40 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
   const setDay = day => setState(prev => ({ ...prev, day }));
 
-  const newAppointments = dailyAppointments.map(app => 
-    <Appointment key={app.id} {...app}/>
-  );
+  const appointments = getAppointmentsForDay(state, state.day);
+
+  const schedule = appointments.map(app => {
+    //getInterview returns an object that contains the interview data if it is passed an object that contains an interviewer
+    const interview = getInterview(state, app.interview);
+    return (
+      <Appointment key={app.id} {...app}/>
+    )    
+  });
 
   useEffect(() => {
     Promise.all([
       axios.get('/api/days'),
       axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ])
     .then((all) => {
+      console.log(all[2].data);
       setState(prev => ({
         ...prev,
         days: all[0].data,
-        appointments: all[1].data}));
+        appointments: all[1].data,
+        interviewers: all[2].data}));
     });
   }, []);
 
@@ -57,7 +66,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-       {newAppointments}
+       {schedule}
        <Appointment key="last" time="5pm" />
       </section>
     </main>
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,6 @@
-//Selector accepts state as an argument and returns data that is derived from that state
+//Selectors as helpers accept state as an argument and return data that is derived from that state
+
+//returns an array of appointments for that day
 export function getAppointmentsForDay(state, day) {
   let newAppointments = [];
 
@@ -19,5 +21,32 @@ export function getAppointmentsForDay(state, day) {
     }
   })
   //console.log('newAppointments', newAppointments);
-  return newAppointments; //returns an array of appointments for that day
+  return newAppointments;
+}
+
+//returns an object with interview data when there is an interviewer in state object; otherwise, returns null
+export function getInterview(state, interview) {
+  //console.log('interview', interview)
+  let newInterview = {};
+  
+  if (interview === null) {
+    //console.log('[appointment].interview', [appointment].interview);
+    return null;
+  } else {
+    newInterview.student = interview.student;
+    //console.log('newInterview.student', newInterview.student);
+
+    let interviewerId = interview.interviewer;
+    //console.log('interviewerId', interviewerId);
+
+    for (let interviewer in state.interviewers) {
+      //console.log('interviewer', interviewer);  
+      if (interviewer == interviewerId) {
+        newInterview.interviewer = state.interviewers[interviewer];
+        //console.log('newInterview.interviewer', newInterview.interviewer);
+      }  
+    }
+  }
+  //console.log(newInterview)
+  return newInterview;
 }
