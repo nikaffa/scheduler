0b176403e160f2946103001bec90fd8fe2f7bf33
Veronika Fatikhova <fidelio@list.ru>
2021-12-12 22:57:20 -0800

    Implement using useVisualMode hook in Application component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,25 +2,28 @@ import React from "react";
 import Empty from "./Empty";
 import Header from "./Header";
 import Show from "./Show";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
 import "./styles.scss";
 
-
 const Appointment = (props) => {
-  // const showAppointment = () => {
-  //   let text ="";
-  //   if (props.time) {
-  //     text = `Appointment at ${props.time}`;
-  //   } else {
-  //     text = "No appointments";
-  //   }
-  //   return text;
-  // }
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  //conditinally renders a mode within calling useVisualMode:
+  //if props.interview contains a value, pass useVisualMode the SHOW mode, otherwise, pass EMPTY
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   
   return (
     <article className="appointment">
       <Header time={props.time}/>
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name}/> : <Empty />}
-      
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && <Show student={props.interview.student} interviewer={props.interview.interviewer.name} />}
+      {mode === CREATE && <Form interviewers={[]} onCancel={() => back(EMPTY)}/>}
     </article>
   
   );
