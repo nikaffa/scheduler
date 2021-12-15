c81442f27f43d45a9a0a595bfef6beef1862659d
Veronika Fatikhova <fidelio@list.ru>
2021-12-15 14:16:14 -0800

    Implement Appointment test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,13 @@
 import React from "react";
 
+//The render function allows to render Components
 import { render, cleanup } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
+//A test that renders a React Component
 it("renders without crashing", () => {
   render(<Application />);
 });
