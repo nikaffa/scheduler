17b6f74b5d7f26dd354fa86a41b6f1f36cab50f2
Veronika Fatikhova <fidelio@list.ru>
2021-12-15 16:51:47 -0800

    Implement Form Tests, make changes in Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,7 @@ import InterviewerList from "components/InterviewerList";
 const Form = (props) => {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setStudent("");
@@ -14,6 +15,13 @@ const Form = (props) => {
   const cancel = () => {
     reset();
     props.onCancel();
+  }
+  const validate = () => {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    props.onSave(student, interviewer);
   }
 
   return (
@@ -27,8 +35,11 @@ const Form = (props) => {
             placeholder="Enter Student Name"
             value = {student}
             onChange = {(e) => {setStudent(e.target.value)}}
+            data-testid="student-name-input"
           />
+          <section className="appointment__validation">{error}</section>          
         </form>
+
         <InterviewerList 
           interviewer = {interviewer}
           value = {interviewer}
@@ -39,7 +50,7 @@ const Form = (props) => {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => props.onSave(student, interviewer)} >Save</Button>
+          <Button confirm onClick={validate} >Save</Button>
         </section>
       </section>
     </main>
